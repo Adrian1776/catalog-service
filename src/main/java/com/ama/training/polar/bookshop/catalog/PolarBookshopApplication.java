e89d7c4724f3d3745ce07f2fe0af242f4fbf5748
Adrian Marc <adrian.marc95@gmail.com>
2023-03-08 09:41:00 +0000

    Initialize Catalog-Service repository

---
+++ src/main/java/com/ama/training/polar/bookshop/catalog/PolarBookshopApplication.java
@@ -0,0 +1,13 @@
+package com.ama.training.polar.bookshop.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolarBookshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolarBookshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ama/training/polar/bookshop/catalog/PolarBookshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ama.training.polar.bookshop.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolarBookshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
